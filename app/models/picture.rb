d0f51f64fd93a0ad1d887a11546739292198a1d8
alexander <alexander.sysuiev@gmail.com>
2011-10-27 23:50:02 +0300

    Configure bucket.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@ class Picture < ActiveRecord::Base
 	has_one :boiler
 	has_attached_file	:photo,
 					    :storage => :s3,
-					    :bucket => 'mybucket',
+					    :bucket => 'webstore-production',
 					    :s3_credentials => {
 					      :access_key_id => ENV['S3_KEY'],
 					      :secret_access_key => ENV['S3_SECRET']
--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,3 +2,9 @@
 #   :access_key_id     => ENV['S3_KEY'],
 #   :secret_access_key => ENV['S3_SECRET']
 # )
+
+# AKIAJ7B33CX46N5YJJOQ
+# c7EANSYaJZG+QjpO5TFzQndknBg8VNEIuifYwee0
+
+# AWS::S3::Base
+S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "webstore-production"}
