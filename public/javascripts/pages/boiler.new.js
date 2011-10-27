d9ad6c90d26a6c5d4e2885c501e3a02d49889557
alexander <alexander.sysuiev@gmail.com>
2011-10-27 21:07:18 +0300

    Remove link was added,

--- public/javascripts/pages/boiler.new.js
+++ public/javascripts/pages/boiler.new.js
@@ -1,9 +1,18 @@
 $(function() {
+	var removeSetting = function() {
+		$(this).parent().remove();
+		return false;
+	};
+
 	$(".setting-add-link").click(function () {
-		div = $('#setting-prototype').clone().removeAttr('id');
-		console.log(div);
-		div.show().appendTo('#settings-area');
+		div = $('#setting-prototype').clone().removeAttr('id').show();
+		div.find('.remove-setting').click(removeSetting);
+		div.appendTo('#settings-area');
+
 		return false;	
 	});
+
+	$(".remove-setting").click(removeSetting);
+	
 	
 });
