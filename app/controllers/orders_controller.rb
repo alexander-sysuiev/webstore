39848dd12b6717076a5dbb10126626edaf069bb2
alexander <alexander.sysuiev@gmail.com>
2011-11-04 08:39:30 +0200

    Orders were added to services.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,6 @@ class OrdersController < WorkflowController
 			type[1].each do |product|
 				good = type[0].camelize.constantize.find(product[0])
 				quantity = product[1].to_i
-				p type[0].inspect
 				BoilerOrder.create(:good_id => good.id, :price => good.price, :quantity => quantity, :type => type[0].to_s, :order_id => order.id)
 			end
 		end
