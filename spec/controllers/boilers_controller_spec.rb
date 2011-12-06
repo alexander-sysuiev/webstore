770a2d998766c9384613d04e4cc989d72991040f
alexander <alexander.sysuiev@gmail.com>
2011-12-07 01:28:12 +0200

    BoilersController specs were changed.

--- spec/controllers/boilers_controller_spec.rb
+++ spec/controllers/boilers_controller_spec.rb
@@ -4,9 +4,16 @@ describe BoilersController do
 	context "#index" do
 		before do
 			@category = Factory :category
+			@boiler = Factory :boiler, :category => @category
 		end
-		it "should work" do
+
+		it "should successfuly do index" do
 			get :index, :category => @category
+			response.should be_success
+		end
+
+		it "should successfuly do show" do
+			get :show, :id => @boiler.id, :category_id => @category.id
 			response.should be_success
 		end
 	end
